52a520a9df34513dc57f72d5a79c364e4311b691
Matt Brauner <mb4828@users.noreply.github.com>
2022-05-06 16:08:55 -0400

    Initial Commit

---
+++ assets/scripts/initSplide.js
@@ -0,0 +1,10 @@
+new Splide( '.splide', {
+    width: '100%',
+    height: 'clamp(300px, 75vh, 700px)',
+    start: Math.floor(Math.random() * 5),
+    type: 'fade',
+    autoplay: true,
+    rewind: true,
+    speed: 3000,
+    interval: 7000
+}).mount();
