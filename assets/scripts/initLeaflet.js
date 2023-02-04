f1bb906d1964820753e82c14709a87b7bc221686
Matt Brauner <mattbrauner44@gmail.com>
2023-02-03 19:59:09 -0500

    Improved rendering for filter by hub

--- assets/scripts/initLeaflet.js
+++ assets/scripts/initLeaflet.js
@@ -3,33 +3,44 @@
 var MAP;
 var LINES;
 
-const HUB_ICON = L.icon({
-    iconUrl: 'assets/images/marker-accent.png',
-    iconSize:     [30, 30], // size of the icon
-    iconAnchor:   [15, 30], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, -30] // point from which the popup should open relative to the iconAnchor
-});
+const HUB_ICON = {
+	icon: L.icon({
+	    iconUrl: 'assets/images/marker-accent.png',
+	    iconSize:     [30, 30], // size of the icon
+	    iconAnchor:   [15, 30], // point of the icon which will correspond to marker's location
+	    popupAnchor:  [0, -30], // point from which the popup should open relative to the iconAnchor
+	}),
+	zIndexOffset: 1000
+};
 
-const MARKER_ICON = L.icon({
-    iconUrl: 'assets/images/marker-primary.png',
-    iconSize:     [20, 20], // size of the icon
-    iconAnchor:   [10, 20], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, -20] // point from which the popup should open relative to the iconAnchor
-});
+const MARKER_ICON = {
+	icon: L.icon({
+	    iconUrl: 'assets/images/marker-primary.png',
+	    iconSize:     [20, 20], // size of the icon
+	    iconAnchor:   [10, 20], // point of the icon which will correspond to marker's location
+	    popupAnchor:  [0, -20] // point from which the popup should open relative to the iconAnchor
+	})
+};
 
-const MARKER_ICON_SECONDARY = L.icon({
-	iconUrl: 'assets/images/marker-secondary.png',
-    iconSize:     [18, 18], // size of the icon
-    iconAnchor:   [9, 18], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, -18] // point from which the popup should open relative to the iconAnchor
-})
+const MARKER_ICON_SECONDARY = {
+	icon: L.icon({
+		iconUrl: 'assets/images/marker-secondary.png',
+	    iconSize:     [20, 20], // size of the icon
+	    iconAnchor:   [10, 20], // point of the icon which will correspond to marker's location
+	    popupAnchor:  [0, -20],// point from which the popup should open relative to the iconAnchor
+	}),
+	zIndexOffset: -1000,
+	opacity: 0.4
+};
 
-const FLOWER_ICON = L.icon({
-	iconUrl: 'assets/images/livery/mb_aloha_flower.png',
-    iconSize:     [18, 18], // size of the icon
-    iconAnchor:   [9, 18], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, -18] // point from which the popup should open relative to the iconAnchor
-})
+const FLOWER_ICON = {
+	icon: L.icon({
+		iconUrl: 'assets/images/livery/mb_aloha_flower.png',
+	    iconSize:     [18, 18], // size of the icon
+	    iconAnchor:   [9, 18], // point of the icon which will correspond to marker's location
+	    popupAnchor:  [0, -18] // point from which the popup should open relative to the iconAnchor
+	})
+};
 
 function drawGeodesic(map, pos1, pos2) {
 	const line = L.geodesic([pos1, pos2], {weight: 2, opacity: 0.4, color: 'var(--primary-color)'}).addTo(map);
@@ -37,24 +48,24 @@ function drawGeodesic(map, pos1, pos2) {
 }
 
 function drawMarker(map, dest, options={}) {
-	let icon = MARKER_ICON;
+	let markerOpts = MARKER_ICON;
 	if (dest.isHub) {
-		icon = HUB_ICON;
+		markerOpts = HUB_ICON;
 	} else if (dest.city.includes('Hawaii')) {
-		icon = FLOWER_ICON;
-	} else if (dest.city.includes('🇺🇸') && dest.hubs.length <= 2) {
-		icon = MARKER_ICON_SECONDARY;
+		markerOpts = FLOWER_ICON;
+	} else if (options.hub && !dest.hubs.includes(options.hub)) {
+		markerOpts = MARKER_ICON_SECONDARY;
 	}
-	const marker = L.marker(dest.pos, {icon: icon}).addTo(map);
+	const marker = L.marker(dest.pos, markerOpts).addTo(map);
 
 	const caption = `&#9992; ${dest.isHub ? 'Hub - ' : ''}${dest.name}<br><small>${dest.city}</small><hr><a href="#book">Book a trip ></a>`;
 	marker.bindPopup(caption);
 
-	if (!document.getElementById('hub-select').value) {
+	if (!options.hub) {
 		// when hub is not selected, show connections on mouseover
 		marker.on('mouseover', onMarkerMouseover);
 		marker.on('mouseout', onMarkerMouseout);
-	} else if (options.originPos) {
+	} else if (options.originPos && dest.hubs.includes(options.hub)) {
 		// when hub is selected, connect hub to all destinations and don't allow mouseover
 		drawGeodesic(map, options.originPos, dest.pos); 
 	}
@@ -83,17 +94,15 @@ function drawMap(hub) {
 	    accessToken: 'pk.eyJ1IjoibWI0ODI4IiwiYSI6ImNsMnFweGpuYTAwNXAzY3Bob3lqaG9rMG4ifQ.O8yYprih5CDw3tH0bDrCHw'
 	}).addTo(MAP);
 
-	const originPos = hub ? DESTINATIONS.find(h => h.code === hub).pos : null;
-	const destList = DESTINATIONS.filter(d => !hub || d.hubs.includes(hub) || d.code === hub);
-	const posList = destList.map(d => d.pos);
-
 	// draw destinations
-	for (const d of destList) {
-		drawMarker(MAP, d, {originPos});
+	const originPos = hub ? DESTINATIONS.find(h => h.code === hub).pos : null;
+	for (const d of DESTINATIONS) {
+		drawMarker(MAP, d, {hub, originPos});
 	}
 
 	// set view
-	MAP.fitBounds(posList);
+	const destList = DESTINATIONS.filter(d => !hub || d.hubs.includes(hub) || d.code === hub);
+	MAP.fitBounds(destList.map(d => d.pos));
 }
 
 function onMarkerMouseover(event) {
