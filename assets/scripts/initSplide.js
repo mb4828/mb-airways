ad5342daa49ec1cf5e7a4695058a1643739667ca
Matt Brauner <mattbrauner44@gmail.com>
2023-10-31 12:14:39 -0400

    Adding 789

--- assets/scripts/initSplide.js
+++ assets/scripts/initSplide.js
@@ -1,31 +1,40 @@
 'use strict';
 
-new Splide( '.lead-splide', {
-    width: '100%',
-    height: 'clamp(300px, 75vh, 700px)',
-    start: Math.floor(Math.random() * 5),
-    type: 'fade',
-    autoplay: true,
-    rewind: true,
-    speed: 2500,
-    interval: 8000,
-    easing: 'ease'
+new Splide('.lead-splide', {
+  width: '100%',
+  height: 'clamp(300px, 75vh, 700px)',
+  start: Math.floor(Math.random() * 5),
+  type: 'fade',
+  autoplay: true,
+  rewind: true,
+  speed: 2500,
+  interval: 8000,
+  easing: 'ease',
 }).mount();
 
-new Splide( '.fleet-table-splide-1', {
-    width: '500px',
-    height: '150px',
-    type: 'loop',
-    autoplay: true,
-    speed: 4000,
-    interval: 7000,
+new Splide('.fleet-table-splide-0', {
+  width: '500px',
+  height: '150px',
+  type: 'loop',
+  autoplay: true,
+  speed: 4000,
+  interval: 7000,
 }).mount();
 
-new Splide( '.fleet-table-splide-2', {
-    width: '500px',
-    height: '150px',
-    type: 'loop',
-    autoplay: true,
-    speed: 4000,
-    interval: 7000
+new Splide('.fleet-table-splide-1', {
+  width: '500px',
+  height: '150px',
+  type: 'loop',
+  autoplay: true,
+  speed: 4000,
+  interval: 7000,
+}).mount();
+
+new Splide('.fleet-table-splide-2', {
+  width: '500px',
+  height: '150px',
+  type: 'loop',
+  autoplay: true,
+  speed: 4000,
+  interval: 7000,
 }).mount();
